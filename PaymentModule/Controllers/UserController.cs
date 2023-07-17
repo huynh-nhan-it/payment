23e88e448f6518dc1781e4b219d0c0551be2ee1a
huynh-nhan-it <128890299+huynh-nhan-it@users.noreply.github.com>
2023-07-17 10:59:27 +0700

    fix-code

--- PaymentModule/Controllers/UserController.cs
+++ PaymentModule/Controllers/UserController.cs
@@ -1,13 +1,32 @@
 ﻿/*using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+<<<<<<< HEAD
 using Microsoft.Data.SqlClient;
 using PaymentModule.DTOs;
 using PaymentModule.Repository;
+=======
+<<<<<<< HEAD
+using Microsoft.EntityFrameworkCore;
+using PaymentModule.Context;
+using PaymentModule.Models;
+using PaymentModule.Entities;
+=======
+using Microsoft.Data.SqlClient;
+using PaymentModule.DTOs;
+using PaymentModule.Repository;
+>>>>>>> origin/export-excel
+>>>>>>> parent of 0743d1f (Fix code create-request)
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+<<<<<<< HEAD
+=======
+using System.Net.Mail;
+using System.Dynamic;
+using PaymentModule.Service;
+>>>>>>> parent of 0743d1f (Fix code create-request)
 
 namespace PaymentModule.Controllers
 {
@@ -15,6 +34,60 @@ namespace PaymentModule.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+<<<<<<< HEAD
+=======
+
+        public static List<UserModel> users = new List<UserModel>();
+        public static List<AccountModel> accounts = new List<AccountModel>();
+        private readonly IFileService _fileService;
+        private PaymentContext _context;
+
+        public UserController(PaymentContext paymentContext)
+        {
+            _context = paymentContext;
+            _fileService = new FileService();
+        }
+
+        [HttpPost("handleFiles")]
+        public async Task<IActionResult> handleFile([FromForm] IFormFileCollection files, Guid Id)
+        {
+            var result = await _fileService.HandleFile(files, Id);
+            var data = result.Value as dynamic;
+            if (data.Success)
+            {
+                return Ok(data.data);
+            }
+            return BadRequest(data.Message);
+
+        }
+
+        
+        [HttpPost("create-request")]
+        public IActionResult createRequest([FromBody] DetailRequestEntity detailRequestEntity)
+        {
+
+            
+            return Ok();
+
+
+            /*IFormFileCollection files = detailRequestEnity.Attachments;
+            var resultProcessFile = handleFile(files, detailRequestEnity.Id);
+            object attachments = null;
+
+            if (resultProcessFile is OkObjectResult okResult)
+            {
+                var responseData = okResult.Value;
+                attachments = responseData as ICollection<AttachmentEntity>;
+            }
+            else
+            {
+                return resultProcessFile;
+            }
+            
+            return Ok(attachments);*/
+        }
+
+>>>>>>> parent of 0743d1f (Fix code create-request)
         private string connectionString = "Data Source=DESKTOP-3VU8FT9\\SQLEXPRESS01;Initial Catalog=PaymentDB;Integrated Security=True";
         private IUserRepository _userRepository;
         public UserController(IUserRepository userRepository)
---
+++ PaymentModule/Service/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentModule.Service
+{
+    public interface IFileService
+    {
+        Task<ObjectResult> HandleFile(IFormFileCollection files, Guid Id);
+    }
+}
