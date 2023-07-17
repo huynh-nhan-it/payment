cfe0872537adb91f24704f1a668a9c7608b5a055
huynh-nhan-it <128890299+huynh-nhan-it@users.noreply.github.com>
2023-07-17 10:37:07 +0700

    create-personal
    create personal api

---
+++ PaymentModule/Controllers/PersonalController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentModule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonalController : ControllerBase
+    {
+    }
+}
