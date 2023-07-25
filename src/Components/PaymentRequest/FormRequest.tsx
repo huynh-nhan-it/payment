d659ce604fbf32a056323e5c493ee5bfe8d25d7b
hong.nt1 <giangnguyen.011210@gmail.com>
2023-07-25 10:58:13 +0700

    create-request router
    create-request router

--- src/Components/PaymentRequest/FormRequest.tsx
+++ src/Components/PaymentRequest/FormRequest.tsx
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Col, Form, Layout, Row, theme } from 'antd';
-import './RequestDetails.css';
-import { Input, Select } from 'antd';
-import { getSuppliers, getCurrencies, getDepartments, postPaymentRequest } from '../../Services/PaymentRequest/apiForm'
+import React, { useEffect, useState } from "react";
+import { Button, Col, Form, Layout, Row, theme } from "antd";
+import "./RequestDetails.css";
+import { Input, Select } from "antd";
+import {
+  getSuppliers,
+  getCurrencies,
+  getDepartments,
+  postPaymentRequest,
+} from "../../Services/PaymentRequest/apiForm";
 
 const { Content } = Layout;
 const FormItem = Form.Item;
@@ -12,7 +17,7 @@ const FormRequest: React.FC = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
-  
+
   const [form] = Form.useForm();
   //test api
   const [supplierData, setSupplierData] = useState<string[]>([]);
@@ -37,8 +42,6 @@ const FormRequest: React.FC = () => {
     }
   };
 
-
-
   const onSubmit = async () => {
     try {
       const formValues = form.getFieldsValue();
@@ -54,123 +57,106 @@ const FormRequest: React.FC = () => {
 
   return (
     <Layout hasSider>
-      <Layout >
-        <Content className='content-center'>
-          <div style={{ paddingTop: 64, textAlign: 'center', background: colorBgContainer }}>
+      <Layout>
+        <Content className="content-center">
+          <div
+            style={{
+              paddingTop: 64,
+              textAlign: "center",
+              background: colorBgContainer,
+            }}>
             <h1 style={{ fontSize: 28 }}>PAYMENT REQUEST</h1>
-            <div className='row'>
-              <div className='font_text'>
-                <Form
-                  name='paymentRequest'
-                  form={form}
-                  onFinish={onSubmit}
-                >
+            <div className="row">
+              <div className="font_text">
+                <Form name="paymentRequest" form={form} onFinish={onSubmit}>
                   <Row gutter={16}>
-                  <Col span={8}>
-                  <Form.Item
-                    label='Purpose'
-                    name='purpose'
-                    rules={[{ required: true }]}
-                    labelCol={{ span: 24 }} 
-                    wrapperCol={{ span: 24 }}
-                  >
-                    <Input
-                      style={{ width: 200 }}
-                      placeholder="Purpose"
-                    />
-                  </Form.Item>
-                  </Col>
-                  
-                  <Col span={8}>
-                  <Form.Item
-                    label='Department'
-                    name='department'
-                    rules={[{ required: true }]}
-                    labelCol={{ span: 24 }} 
-                    wrapperCol={{ span: 24 }}
-                  >
-                    <Select
-                      style={{ width: 200 }}
-                      key="department"
-                    >
-                      {departmentData.map((departmentName) => (
-                        <Option key={departmentName} value={departmentName}>
-                          {departmentName}
-                        </Option>
-                      ))}
-                    </Select>
-                  </Form.Item>
-                  </Col>
-                  <Col span={8}>
-                  <FormItem
-                    label='Payment For'
-                    name='paymentFor'
-                    rules={[{ required: true }]}
-                    labelCol={{ span: 24 }} 
-                    wrapperCol={{ span: 24 }}
-                  >
-                    <Input
-                      style={{ width: 200 }}
-                      placeholder="Payment For"
-                    />
-                  </FormItem>
-                  </Col>
+                    <Col span={8}>
+                      <Form.Item
+                        label="Purpose"
+                        name="purpose"
+                        rules={[{ required: true }]}
+                        labelCol={{ span: 24 }}
+                        wrapperCol={{ span: 24 }}>
+                        <Input style={{ width: 200 }} placeholder="Purpose" />
+                      </Form.Item>
+                    </Col>
+
+                    <Col span={8}>
+                      <Form.Item
+                        label="Department"
+                        name="department"
+                        rules={[{ required: true }]}
+                        labelCol={{ span: 24 }}
+                        wrapperCol={{ span: 24 }}>
+                        <Select style={{ width: 200 }} key="department">
+                          {departmentData.map((departmentName) => (
+                            <Option key={departmentName} value={departmentName}>
+                              {departmentName}
+                            </Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <FormItem
+                        label="Payment For"
+                        name="paymentFor"
+                        rules={[{ required: true }]}
+                        labelCol={{ span: 24 }}
+                        wrapperCol={{ span: 24 }}>
+                        <Input
+                          style={{ width: 200 }}
+                          placeholder="Payment For"
+                        />
+                      </FormItem>
+                    </Col>
                   </Row>
                   <Row gutter={16}>
-                  <Col span={8}>
-                  <Form.Item
-                    label='Supplier'
-                    name='supplier'
-                    rules={[{ required: true }]}
-                    labelCol={{ span: 24 }} 
-                    wrapperCol={{ span: 24 }}
-                  >
-                    <Select
-                      style={{ width: 200 }}
-                      key="supplier"                      
-                    >
-                      {supplierData.map((supplierName) => (
-                        <Option key={supplierName} value={supplierName}>
-                          {supplierName}
-                        </Option>
-                      ))}
-                    </Select>
-                  </Form.Item>
-                  </Col>
-                  <Col span={8}>
-                  <Form.Item
-                    label='Currency'
-                    name='currency'
-                    rules={[{ required: true }]}
-                    labelCol={{ span: 24 }} 
-                    wrapperCol={{ span: 24 }}
-                  >
-                    <Select
-                      style={{ width: 200 }}
-                      key="currency"                      
-                    >
-                      {currencyData.map((currencyName) => (
-                        <Option key={currencyName} value={currencyName}>
-                          {currencyName}
-                        </Option>
-                      ))}
-                    </Select>
-                  </Form.Item>
-                  </Col>
-                  <Col span={8}>
-                  <FormItem
-                    label='PO/PR number'
-                    name='poPrNumber'
-                    rules={[{ required: true }]}
-                    labelCol={{ span: 24 }} 
-                    wrapperCol={{ span: 24 }}
-                  >
-                    <Input
-                      style={{ width: 200 }}
-                      placeholder="PO/PR number"
-                    />
-                  </FormItem>
-                  </Col>
+                    <Col span={8}>
+                      <Form.Item
+                        label="Supplier"
+                        name="supplier"
+                        rules={[{ required: true }]}
+                        labelCol={{ span: 24 }}
+                        wrapperCol={{ span: 24 }}>
+                        <Select style={{ width: 200 }} key="supplier">
+                          {supplierData.map((supplierName) => (
+                            <Option key={supplierName} value={supplierName}>
+                              {supplierName}
+                            </Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <Form.Item
+                        label="Currency"
+                        name="currency"
+                        rules={[{ required: true }]}
+                        labelCol={{ span: 24 }}
+                        wrapperCol={{ span: 24 }}>
+                        <Select style={{ width: 200 }} key="currency">
+                          {currencyData.map((currencyName) => (
+                            <Option key={currencyName} value={currencyName}>
+                              {currencyName}
+                            </Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                      <FormItem
+                        label="PO/PR number"
+                        name="poPrNumber"
+                        rules={[{ required: true }]}
+                        labelCol={{ span: 24 }}
+                        wrapperCol={{ span: 24 }}>
+                        <Input
+                          style={{ width: 200 }}
+                          placeholder="PO/PR number"
+                        />
+                      </FormItem>
+                    </Col>
                   </Row>
                   {/* <Button onClick={() => form.submit()}>Submit</Button> */}
                 </Form>
--- src/Components/PaymentRequest/SubmitRequest.tsx
+++ src/Components/PaymentRequest/SubmitRequest.tsx
@@ -1,23 +1,64 @@
-import React from 'react';
-import FormRequest from './FormRequest';
-import TableRequest from './TableRequest';
-import axios from 'axios';
-import dayjs from 'dayjs';
-import Item from 'antd/es/descriptions/Item.js';
-import AttachmentRequest from './AttachmentRequest';
-import ApproverRequest from './ApproverRequest';
+import React, { useState } from "react";
+import FormRequest from "./FormRequest";
+import TableRequest from "./TableRequest";
+import AttachmentRequest from "./AttachmentRequest";
+import ApproverRequest from "./ApproverRequest";
+import { Layout, theme } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { Content } from "antd/es/layout/layout";
+import HeaderRequest from "../Request/HeaderRequest";
+import NavbarRequest from "../Request/NavbarRequest";
+import Search from "antd/es/input/Search";
+import HeaderCreateRequest from "./Header";
 
 const ParentComponent = () => {
-
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div style={{paddingTop: 64}} >
-      <FormRequest  />
-      <TableRequest onChange={function (data: { paymentMethod: string; }): void {
-        throw new Error('Function not implemented.');
-      } } />
-      <AttachmentRequest/>
-      <ApproverRequest/>
-    </div>  
+    <Layout>
+      <HeaderRequest />
+      <Content>
+        <Layout>
+          <Sider
+            className="sider-request"
+            style={{
+              background: colorBgContainer,
+              padding: "64px 0",
+              position: "fixed",
+              height: "100%",
+              borderRight: "solid #ccc 0.1px",
+            }}
+            // collapsedWidth="0"
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(value) => setCollapsed(value)}>
+            <Search
+              placeholder="input search text"
+              // onSearch={onSearch}
+              style={{
+                width: 200,
+              }}
+            />
+            <NavbarRequest />
+          </Sider>
+          <Content
+            style={{ paddingLeft: collapsed ? "0" : "200px", marginTop: "100px" }}
+            className="content-request">
+              <HeaderCreateRequest/>
+            <FormRequest />
+            <TableRequest
+              onChange={function (data: { paymentMethod: string }): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+            <AttachmentRequest />
+            <ApproverRequest />
+          </Content>
+        </Layout>
+      </Content>
+    </Layout>
   );
 };
 
