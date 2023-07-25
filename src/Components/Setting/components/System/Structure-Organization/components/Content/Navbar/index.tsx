591ff8719c529d520d88fac0014b79708fae2497
hong.nt1 <giangnguyen.011210@gmail.com>
2023-07-25 11:13:17 +0700

    router for department
    router for department

--- src/Components/Setting/components/System/Structure-Organization/components/Content/Navbar/index.tsx
+++ src/Components/Setting/components/System/Structure-Organization/components/Content/Navbar/index.tsx
@@ -6,7 +6,6 @@ import { getDepartments } from "../../../../../../../../Services/PaymentRequest/
 import { getDepartmentUsers } from "../../../../../../../../Services/Organizations/apiDepartmentUsers";
 
 const NavbarDepartment = () => {
-
   interface User {
     fullName: string;
     email: string;
@@ -17,34 +16,30 @@ const NavbarDepartment = () => {
   const [supervisor, setSupervisor] = useState<User[]>();
   const [employees, setEmployees] = useState<User[]>();
 
-
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-  try {
-    
-    const departmentResponse = await getDepartments();
+    try {
+      const departmentResponse = await getDepartments();
 
-    
-    setDepartmentData(departmentResponse);
-  } catch (error) {
-    console.error(error);
-  }
-};
+      setDepartmentData(departmentResponse);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const [nameDepartment, setNameDepartment] = useState<any | null>(null);
-const [descriptionDepartment, setDecriptionDepartment] = useState<any | null>(null);
-const [underDepartment, setUnderDepartmen] = useState<any | null>(null);
-const [contactDepartment, setContactDepartmen] = useState<any | null>(null);
-const [codeDepartment, setCodeDepartmen] = useState<any | null>(null);
-
+  const [nameDepartment, setNameDepartment] = useState<any | null>(null);
+  const [descriptionDepartment, setDecriptionDepartment] = useState<any | null>(
+    null
+  );
+  const [underDepartment, setUnderDepartmen] = useState<any | null>(null);
+  const [contactDepartment, setContactDepartmen] = useState<any | null>(null);
+  const [codeDepartment, setCodeDepartmen] = useState<any | null>(null);
 
   const handleDepartmentClick = async (department: any) => {
     setNameDepartment(department);
-    
-
 
     try {
       // Gọi hàm getDepartmentUsers để lấy danh sách người dùng của phòng ban
@@ -57,114 +52,102 @@ const [codeDepartment, setCodeDepartmen] = useState<any | null>(null);
       setManager(data.manager);
       setSupervisor(data.supervisors);
       setEmployees(data.employees);
-      
-
-
     } catch (error) {
       console.error(error);
     }
-
-    
   };
   console.log(manager);
-  
-
 
   const [container, setContainer] = useState<HTMLDivElement | null>(null);
 
-
   return (
-
-      <Row>
+    <Row>
       <Col span={8}>
-    <div className="navbar-department">
-      <div className="search-department">
-      <Input placeholder="Search" />
-      </div>
-      <div className="scrollable-container" >
-        <div className="background">
-          <Affix target={() => container}>
-            <ul>
-              {departmentData.map((dep) => (
-                <li onClick={() => handleDepartmentClick(dep)}
-                className="department-list"
-                key={dep}>
-                  <a className="org-department display-flex color-black" href="#">
-                    <span className="org-department-icon">
-                      <MdOutlineGroups />
-                    </span>
-                    <p className="org-department-name">{dep}</p>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </Affix>
+        <div className="navbar-department">
+          <div className="search-department">
+            <Input placeholder="Search" />
+          </div>
+          <div className="scrollable-container">
+            <div className="background">
+              <Affix target={() => container}>
+                <ul>
+                  {departmentData.map((dep) => (
+                    <li
+                      onClick={() => handleDepartmentClick(dep)}
+                      className="department-list"
+                      key={dep}>
+                      <a
+                        className="org-department display-flex color-black"
+                        href="#">
+                        <span className="org-department-icon">
+                          <MdOutlineGroups />
+                        </span>
+                        <p className="org-department-name">{dep}</p>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </Affix>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    </Col>
-    <Col span={16}>
-      <div style={{paddingTop: 64}} className="details-department">
-        {nameDepartment && (
-          <>
-            <h2 style={{textAlign:"left", fontSize: 40}}>{nameDepartment}</h2>
+      </Col>
+      <Col span={16}>
+        <div style={{ paddingTop: 64 }} className="details-department">
+          {nameDepartment && (
+            <>
+              <h2 style={{ textAlign: "left", fontSize: 40 }}>
+                {nameDepartment}
+              </h2>
               <Tabs defaultActiveKey="1">
                 <Tabs.TabPane tab="Information" key="1">
                   <List>
                     <List.Item>
-                  {
-                    <tr>
-                      <td className="title-details">
-                        Description
-                      </td>
-                      <td className="content-details">
-                      <p>{descriptionDepartment}</p>
-                      </td>
-                  </tr>               
-                  }
+                      {
+                        <tr>
+                          <td className="title-details">Description</td>
+                          <td className="content-details">
+                            <p>{descriptionDepartment}</p>
+                          </td>
+                        </tr>
+                      }
                     </List.Item>
                     <List.Item>
-                  {
-                    
-                    <tr>
-                      <td className="title-details">
-                        Under the department	
-                      </td>
-                      <td className="content-details">
-                      <p>{underDepartment}</p>
-                      </td>
-                    </tr>
-                  }
+                      {
+                        <tr>
+                          <td className="title-details">
+                            Under the department
+                          </td>
+                          <td className="content-details">
+                            <p>{underDepartment}</p>
+                          </td>
+                        </tr>
+                      }
                     </List.Item>
                     <List.Item>
-                    {
-                    <tr>
-                      <td className="title-details">
-                        Contact Info	
-                      </td>
-                      <td className="content-details">
-                      <p>{contactDepartment}</p>
-
-                      </td>
-                    </tr>
-                  }
+                      {
+                        <tr>
+                          <td className="title-details">Contact Info</td>
+                          <td className="content-details">
+                            <p>{contactDepartment}</p>
+                          </td>
+                        </tr>
+                      }
                     </List.Item>
                     <List.Item>
-                    {
-                    <tr>
-                      <td className="title-details">
-                        Code
-                      </td>
-                      <td className="content-details">
-                      <p>{codeDepartment}</p>
-                      </td>
-                    </tr>
-                  }
-                  </List.Item>
+                      {
+                        <tr>
+                          <td className="title-details">Code</td>
+                          <td className="content-details">
+                            <p>{codeDepartment}</p>
+                          </td>
+                        </tr>
+                      }
+                    </List.Item>
                   </List>
-                </Tabs.TabPane>                                
-                <Tabs.TabPane tab="Manager" key="2">                                
-                  {manager &&(
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Manager" key="2">
+                  {manager && (
                     <List>
                       <List.Item>
                         <p>{manager.fullName}</p>
@@ -173,38 +156,36 @@ const [codeDepartment, setCodeDepartmen] = useState<any | null>(null);
                       </List.Item>
                     </List>
                   )}
-                                  
                 </Tabs.TabPane>
                 <Tabs.TabPane tab="Supervisor" key="3">
                   <List>
-                  {supervisor?.map((user) => (
-                    <List.Item>
-                    <p>{user.fullName}</p>
-                    <p>{user.email}</p>
-                    <p>{user.jobTitle}</p>
-                    </List.Item>
-                  ))}
+                    {supervisor?.map((user) => (
+                      <List.Item>
+                        <p>{user.fullName}</p>
+                        <p>{user.email}</p>
+                        <p>{user.jobTitle}</p>
+                      </List.Item>
+                    ))}
                   </List>
                 </Tabs.TabPane>
                 <Tabs.TabPane tab="Employee" key="4">
-                <List>
-                  {employees?.map((user) => (
-                    <List.Item>
-                    <p>{user.fullName}</p>
-                    <p>{user.email}</p>
-                    <p>{user.jobTitle}</p>
-                    </List.Item>
-                  ))}
+                  <List>
+                    {employees?.map((user) => (
+                      <List.Item>
+                        <p>{user.fullName}</p>
+                        <p>{user.email}</p>
+                        <p>{user.jobTitle}</p>
+                      </List.Item>
+                    ))}
                   </List>
                 </Tabs.TabPane>
               </Tabs>
-          </>
+            </>
           )}
-          
-      </div>
-    </Col>
-  </Row>
-);
+        </div>
+      </Col>
+    </Row>
+  );
 };
 
 export default NavbarDepartment;
--- src/Components/Setting/components/System/Structure-Organization/components/StructureOrganization.tsx
+++ src/Components/Setting/components/System/Structure-Organization/components/StructureOrganization.tsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderOrganize from "./Header/Header";
 import NavbarDepartment from "./Content/Navbar";
+import { Layout, theme } from "antd";
+import Sider from "antd/es/layout/Sider";
+import { Content } from "antd/es/layout/layout";
+import HeaderRequest from "../../../../../Request/HeaderRequest";
+import NavbarRequest from "../../../../../Request/NavbarRequest";
+import Search from "antd/es/input/Search";
 
 const StructureOrganization = () => {
+  const handleClickCard = () => {};
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div style={{ paddingTop: 64 }}>
-      <HeaderOrganize />
-      <NavbarDepartment/>
-      
+    <div>
+      <Layout>
+        <HeaderRequest />
+        <Content>
+          <Layout>
+            <Sider
+              className="sider-request"
+              style={{
+                background: colorBgContainer,
+                padding: "64px 0",
+                position: "fixed",
+                height: "100%",
+                borderRight: "solid #ccc 0.1px",
+              }}
+              // collapsedWidth="0"
+              collapsible
+              collapsed={collapsed}
+              onCollapse={(value) => setCollapsed(value)}>
+              <Search
+                placeholder="input search text"
+                // onSearch={onSearch}
+                style={{
+                  width: 200,
+                }}
+              />
+              <NavbarRequest />
+            </Sider>
+            <Content
+              style={{
+                paddingLeft: collapsed ? "0" : "200px",
+                marginTop: "100px",
+              }}
+              className="content-request">
+              <HeaderOrganize />
+              <NavbarDepartment />
+            </Content>
+          </Layout>
+        </Content>
+      </Layout>
     </div>
   );
 };
